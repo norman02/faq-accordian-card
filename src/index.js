0ff32df584ed38ba87e4758008c7d07bc105c37d
John N. Norman <normanj777@gmail.com>
2021-02-17 07:18:10 -0500

    add basic styling for mobile

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './css/main.css'
 
-// How many team members can I invite? You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan. What is the maximum file upload size? No more than 2GB. All files in your account must fit your allotted storage space. How do I reset my password? Click “Forgot password” from the login page or “Change password” from your profile page. A reset link will be emailed to you. Can I cancel my subscription? Yes! Send us a message and we’ll process your request no questions asked. Do you provide additional support? Chat and email support is available 24/7. Phone lines are open during normal business hours.
+
 
 const questions = [
     {
@@ -38,6 +38,13 @@ class App extends React.Component {
             <main>
                 <div className="faq">
                     <h1>Faq</h1>
+                    <ul>
+                        <li>How many team members can I invite? </li>
+                        <li>What is the maximum file upload size?</li>
+                        <li>How do I reset my password?</li>
+                        <li>Can I cancel my subscription?</li>
+                        <li>Do you provide additional support?</li>
+                    </ul>
                 </div>
 
             </main>
