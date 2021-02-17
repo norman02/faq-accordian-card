93b3c93cd3ace00d49d6de5d6c89f7bda53b78c4
John N. Norman <normanj777@gmail.com>
2021-02-17 08:42:21 -0500

    refactor to populate faqs by mapping the questions array

--- src/index.js
+++ src/index.js
@@ -3,35 +3,41 @@ import ReactDOM from 'react-dom'
 import './css/main.css'
 import downArrow from './images/icon-arrow-down.svg'
 
-
+const arrow = <img src={downArrow} alt='arrow' />
 
 const questions = [
     {
-        number: 1,
         question: 'How many team members can I invite?',
         answer: 'I invite? You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan.',
     },
     {
-        number: 2,
         question: 'What is the maximum file upload size?',
         answer: 'What is the maximum file upload size? No more than 2GB. All files in your account must fit your allotted storage space.',
     },
     {
-        number: 3,
         question: 'How do I reset my password?',
         answer: 'Click “Forgot password” from the login page or “Change password” from your profile page. A reset link will be emailed to you.',
     },
     {
-        number: 4,
         question: 'Can I cancel my subscription?',
         answer: 'Yes! Send us a message and we’ll process your request no questions asked.',
     },
     {
-        number: 5,
         question: 'Do you provide additional support?',
         answer: 'Chat and email support is available 24/7. Phone lines are open during normal business hours.',
     },
 ]
+
+const faqs = questions.map((question) => {
+    return (
+        <li>
+            {question.question}
+            {arrow}
+        </li>
+    )
+
+})
+
 
 class App extends React.Component {
     render() {
@@ -40,11 +46,7 @@ class App extends React.Component {
                 <div className="faq">
                     <h1>Faq</h1>
                     <ul>
-                        <li>How many team members can I invite? <img src={downArrow} alt='arrow'/> </li>
-                        <li>What is the maximum file upload size?<img src={downArrow} alt='arrow'/></li>
-                        <li>How do I reset my password?<img src={downArrow} alt='arrow'/></li>
-                        <li>Can I cancel my subscription?<img src={downArrow} alt='arrow'/></li>
-                        <li>Do you provide additional support?<img src={downArrow} alt='arrow'/></li>
+                        {faqs}
                     </ul>
                 </div>
 
