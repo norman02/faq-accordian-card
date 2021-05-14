634bd8eda4edd02be3627882a3dae9382678116b
John N. Norman <normanj777@gmail.com>
2021-05-14 17:03:15 -0400

    remove test code

--- js/app.js
+++ js/app.js
@@ -1,8 +1,6 @@
 
 document.querySelectorAll('.list-item').forEach(item => {
     item.addEventListener('click', event => {
-      //handle click
-      console.log('click!')
       if (!item.classList.contains('active')) {
           item.classList.add('active')
       } else {
